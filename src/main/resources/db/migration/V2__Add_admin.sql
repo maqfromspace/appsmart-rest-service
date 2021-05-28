6c6677cf1c44c65440336381825e67b79c03dfac
maqfromspace <maqfromspace@gmail.com>
2021-05-29 01:08:11 +0300

    Added table migration with FlyWay #17

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,14 +1,12 @@
 DELETE FROM public.user_roles;
 DELETE FROM public.roles;
+DELETE FROM public.users;
+
 INSERT INTO public.roles(id, name)
 VALUES (1, 'ADMIN_ROLE');
 
-DELETE FROM public.users;
-INSERT INTO public.users(
-    id, created_at, modified_at, password, username)
+INSERT INTO public.users(id, created_at, modified_at, password, username)
 VALUES (1, current_timestamp , current_timestamp , '$2y$12$Ui0bA9j/5xtfjNYxnD094O4M/MNfyYkjolRGYPvlqKp6fYto6GRkC', 'admin');
 
-
-INSERT INTO public.user_roles(
-    user_id, role_id)
+INSERT INTO public.user_roles(user_id, role_id)
 VALUES (1, 1);
